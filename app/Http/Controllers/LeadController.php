19cee548f4fb1b309cdfcdfdf417b7e309a3a840
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2025-06-26 07:53:31 +0300

    fixes

--- app/Http/Controllers/LeadController.php
+++ app/Http/Controllers/LeadController.php
@@ -199,20 +199,17 @@ class LeadController extends Controller
      */
     public function edit(Lead $lead, Request $request)
     {
-<<<<<<< HEAD
-=======
 
         $loggedInUserId = auth()->id();
         $agentsList = User::where('is_active', 1)
             ->select(DB::raw("CONCAT(name, ' - ', agent_code) as name"), 'id')
             ->pluck('name', 'id')
             ->toArray();
-        
+
         // Add "Me" item
-        $agentsList = [$loggedInUserId =>"Myself" ] + $agentsList;
-        
-       // dd($agentsList);
->>>>>>> 25aba04858ba4dafe48e1bc78d0efc8c5ecab38b
+        $agentsList = [$loggedInUserId => "Myself"] + $agentsList;
+
+        // dd($agentsList);
         return view('lead.edit')->with([
             'step' => $request['step'],
             'lead' => $lead,
@@ -227,13 +224,7 @@ class LeadController extends Controller
                 ->select(DB::raw("CONCAT(lead_priority_name, ' - ', description) as name"), 'id')
                 ->pluck('name', 'id'),
             'industries' => LeadIndustry::where('is_active', 1)->pluck('lead_industry_name', 'id'),
-<<<<<<< HEAD
-            'agentsList' => User::where('is_active', 1)
-                ->select(DB::raw("CONCAT(name, ' - ', agent_code) as name"), 'id')
-                ->pluck('name', 'id'),
-=======
             'agentsList' => $agentsList,
->>>>>>> 25aba04858ba4dafe48e1bc78d0efc8c5ecab38b
             'departments' => Department::where('is_active', 1)->pluck('department_name', 'id')
 
         ]);
