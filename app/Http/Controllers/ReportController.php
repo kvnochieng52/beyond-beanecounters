3a74f7f83471af18e63c9bd6ad1d70efabe17bf9
Kevin Ochieng <7-kochieng@users.noreply.github.wananchi.com>
2026-02-08 18:53:50 +0300

    Ringing No response

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -1068,7 +1068,7 @@ class ReportController extends Controller
 
             // 6. Right Party PTP Today - activities with disposition=4 and PTP due today
             $rightPartyPtpQuery = Activity::where('created_by', $agent->id)
-                ->where('act_call_disposition_id', 4)
+                ->where('act_call_disposition_id', 3)
                 ->whereDate('act_ptp_date', Carbon::today());
             if ($institutionId) {
                 $rightPartyPtpQuery->whereHas('lead', function ($q) use ($institutionId) {
